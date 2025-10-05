6b9d3c9e12437a388bada885196f7008450eecbd
Icon1c <rishtiwari98@gmail.com>
2025-10-05 19:30:09 +0200

    adding matrix comprehension

---
+++ Python Refresher/5. Matrix_Transposition_with_Complrehensions.py
@@ -0,0 +1,34 @@
+"""
+5. Matrix Transposition with Comprehensions
+Description: Write a function transpose(matrix) that takes a 2D list (a list of lists) representing a matrix. The function must return the transpose of the matrix. You are required to implement the core logic using a single nested list comprehension.
+Example:
+Input: matrix = [[1, 2, 3], [4, 5, 6]]
+Output: [[1, 4], [2, 5], [3, 6]]
+Constraints:
+You can assume the input matrix is non-empty and rectangular (all rows have the same length).
+"""
+
+def transpose(matrix):
+    row = len(matrix) 
+    col = len(matrix[0])
+    transposed = []
+    
+    for i in range(col):
+        new_row = []
+        for j in range(row):
+            element = matrix[j][i]
+            new_row.append(element)
+        transposed.append(new_row)
+    
+    return transposed
+
+rows = int(input("Enter number of rows: "))
+
+matrix = []
+for i in range(rows):
+    row = list(map(int, input(f"Enter row {i+1} (space separated): ").split()))
+    matrix.append(row)
+
+print("Matrix =", matrix)
+transposed_matrix = transpose(matrix)
+print("Transposed Matrix =", transposed_matrix)
