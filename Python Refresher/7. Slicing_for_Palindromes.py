2560fc79f62646273d24fad960e5d6320c84cccc
Icon1c <rishtiwari98@gmail.com>
2025-10-06 01:37:01 +0200

    Adding slicing problem

---
+++ Python Refresher/7. Slicing_for_Palindromes.py
@@ -0,0 +1,9 @@
+"""
+Description: Write a function is_palindrome_slicing(s) that takes a string s and returns True if the string is a palindrome, and False otherwise. A palindrome is a word that reads the same forwards and backwards. Your implementation must use string slicing to perform the reversal and comparison.
+Example 1:
+Input: s = "racecar"
+Output: True
+Example 2:
+Input: s = "python"
+Output: False
+"""
