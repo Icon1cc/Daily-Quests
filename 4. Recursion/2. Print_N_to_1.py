7342cb6d115b8bfa930433de63010521a11d85e1
Icon1cc <rishtiwari98@gmail.com>
2026-01-03 22:45:38 +0100

    adding problem 2 for recursion

---
+++ 4. Recursion/2. Print_N_to_1.py
@@ -0,0 +1,17 @@
+"""
+Print numbers from N to 1 using recursion.
+"""
+
+def print_N_to_1(n):
+    if n == 0:
+        return
+    print(n)
+    print_N_to_1(n - 1)
+try:
+    N = int(input("Enter a positive integer N: "))
+    if N < 1:
+        print("Please enter a positive integer greater than 0.")
+    else:
+        print_N_to_1(N)
+except ValueError:  
+    print("Invalid input. Please enter a positive integer.")
