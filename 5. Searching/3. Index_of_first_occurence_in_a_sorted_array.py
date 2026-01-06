a42fcedc0166e7898e15060bac42104eccbe6b96
Icon1cc <rishtiwari98@gmail.com>
2026-01-06 23:37:57 +0100

    searching the first index occurence

---
+++ 5. Searching/3. Index_of_first_occurence_in_a_sorted_array.py
@@ -0,0 +1,35 @@
+"""
+Write a python function that finds the index of the first occurrence of a target value in a sorted array.
+If the element is not found, return -1.
+"""
+
+def index_of_first_occurrence(arr, target, left, right):
+    left = 0
+    right = len(arr) - 1
+    if left > right:
+        return -1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif target < arr[mid]:
+            right = mid - 1
+        elif target > arr[mid]:
+            left = mid + 1
+        else:
+            if mid == 0 or arr[mid] != arr[mid - 1]:
+                return mid
+            else:
+                right = mid - 1
+    return -1
+
+try:
+    sorted_list = list(map(int, input("Enter a sorted list of numbers (space-separated): ").split()))
+    target_value = int(input("Enter the target value to search for: "))
+    result = index_of_first_occurrence(sorted_list, target_value, 0, len(sorted_list) - 1)
+    if result != -1:
+        print(f"First occurrence of target value {target_value} found at index {result}.")
+    else:
+        print(f"Target value {target_value} not found in the list.")
+except ValueError:
+    print("Invalid input. Please enter integers only.") 
