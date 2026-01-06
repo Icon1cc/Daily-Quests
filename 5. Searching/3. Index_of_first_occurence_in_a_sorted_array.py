92878e331bb086256d4d869767b3c757e4b480d2
Icon1cc <rishtiwari98@gmail.com>
2026-01-06 23:55:18 +0100

    fixing error

--- 5. Searching/3. Index_of_first_occurence_in_a_sorted_array.py
+++ 5. Searching/3. Index_of_first_occurence_in_a_sorted_array.py
@@ -3,31 +3,29 @@ Write a python function that finds the index of the first occurrence of a target
 If the element is not found, return -1.
 """
 
-def index_of_first_occurrence(arr, target, left, right):
+def index_of_first_occurrence(arr, target):
     left = 0
     right = len(arr) - 1
+    result = -1
+
     while left <= right:
-        mid = (left + right) // 2
+        mid = left + (right - left) // 2
         if arr[mid] == target:
-            return mid
+            result = mid
+            right = mid - 1 
         elif target < arr[mid]:
             right = mid - 1
-        elif target > arr[mid]:
-            left = mid + 1
         else:
-            if mid == 0 or arr[mid] != arr[mid - 1]:
-                return mid
-            else:
-                right = mid - 1
-    return -1
+            left = mid + 1
+    return result
 
 try:
     sorted_list = list(map(int, input("Enter a sorted list of numbers (space-separated): ").split()))
     target_value = int(input("Enter the target value to search for: "))
-    result = index_of_first_occurrence(sorted_list, target_value, 0, len(sorted_list) - 1)
+    result = index_of_first_occurrence(sorted_list, target_value)
     if result != -1:
         print(f"First occurrence of target value {target_value} found at index {result}.")
     else:
         print(f"Target value {target_value} not found in the list.")
 except ValueError:
-    print("Invalid input. Please enter integers only.") 
+    print("Invalid input. Please enter integers only.")
