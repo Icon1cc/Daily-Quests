e43db157d4ff619706242527bf064f0a0e0e7e22
Icon1cc <rishtiwari98@gmail.com>
2026-03-18 13:04:40 +0100

    adding custom function for reverse

--- 12. Stack/07. Prefix to Infix.py
+++ 12. Stack/07. Prefix to Infix.py
@@ -5,19 +5,27 @@ Prefix expression: The expression of the form op a b. When an operator is follow
 Infix expression: The expression of the form a op b. When an operator is in between every pair of operands.
 """
 
+def custom_reverse(expression):
+    temp_stack = []
+    for char in expression:
+        temp_stack.append(char)
+    while temp_stack:
+        yield temp_stack.pop()
+
 def prefix_to_infix(expression):
     stack = []
-    # Reverse the prefix expression to process from right to left
-    for char in reversed(expression):
-        if char.isalnum():  # If the character is an operand (number or variable)
+    # Using our custom reverse function
+    for char in custom_reverse(expression):
+        if char.isalnum():  
             stack.append(char)
-        else:  # The character is an operator
+        else:  
             operand1 = stack.pop()
             operand2 = stack.pop()
             new_expr = f'({operand1} {char} {operand2})'
             stack.append(new_expr)
 
-    return stack[-1]  # The final infix expression will be at the top of the stack
+    return stack[-1] 
+
 test_expression = "*+AB-CD"
 print("Prefix Expression: ", test_expression)
 print("Infix Expression: ", prefix_to_infix(test_expression))
